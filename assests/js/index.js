b0293227e5f3521992b3e10aaae0d953f88d0db6
Wladimir Guevara <wladige@gmail.com>
2021-04-19 22:10:53 -0400

    ejercicio de freecodecamp se usa flex

---
+++ assests/js/index.js
@@ -0,0 +1,9 @@
+// alert('aqui');
+// UpperCase function 
+function upercase() {
+    // alert('jijiji')
+    let str = 'js documentation'
+    let res = str.toUpperCase();
+    document.getElementById('nav-title').innerHTML = res;
+  }
+  upercase();
